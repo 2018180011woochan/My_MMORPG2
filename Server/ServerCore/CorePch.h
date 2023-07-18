a1fef79d55547ad3c2ade5648e8bcb7658d350bf
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2023-07-18 15:49:32 +0900

    개발 환경 설정

---
+++ Server/ServerCore/CorePch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// 실질적으로 게임서버나 클라이언트에서 참조하길 바라는 내용들
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+
+using namespace std;
