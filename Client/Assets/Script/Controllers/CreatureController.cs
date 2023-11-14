92a4ce6c174d806d7b378f8d8610ee66ea1f41a7
DESKTOP-R12LSJN\KWC <wc5427@naver.com>
2023-11-14 22:07:01 +0900

    오브젝트 매니저 생성

---
+++ Client/Assets/Script/Controllers/CreatureController.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Define;
+
+public class CreatureController : MonoBehaviour
+{
+    public float _speed = 5.0f;
+    public Vector3Int CellPos { get; set; } = Vector3Int.zero;
+    protected Animator _animator;
+
+    CreatureState _state = CreatureState.Idle;
+    public CreatureState State
+    { 
+        get { return _state; }
+        set
+        {
+            if (_state == value)
+                return;
+
+            _state = value;
+            UpdateAnimation();
+        }
+    }
+
+    MoveDir _lastDir = MoveDir.Down;
+    MoveDir _dir = MoveDir.Down;
+    public MoveDir Dir
+    {
+        get { return _dir; }
+        set
+        {
+            if (_dir == value)
+                return;
+
+            _dir = value;
+            if (value != MoveDir.None)
+                _lastDir = value;
+            UpdateAnimation();
+        }
+    }
+
+    protected virtual void UpdateAnimation()
+    {
+        if (_state == CreatureState.Idle)
+        {
+            switch (_lastDir)
+            {
+                case MoveDir.Up:
+                    _animator.Play("IDLE_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("IDLE_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("IDLE_RIGHT");
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("IDLE_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+            }
+        }
+        else if (_state == CreatureState.Moving)
+        {
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    _animator.Play("WALK_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Down:
+                    _animator.Play("WALK_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Left:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDir.Right:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+            }
+        }
+        else if (_state == CreatureState.Skill)
+        {
+            // TODO
+        }
+        else
+        {
+
+        }
+    }
+    void Start()
+    {
+        Init();
+        
+    }
+
+    void Update()
+    {
+        UpdateController();
+    }
+
+    protected virtual void Init()
+    {
+        _animator = GetComponent<Animator>();
+        Vector3 pos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, -0.2f);
+        transform.position = pos;
+    }
+
+    protected virtual void UpdateController()
+    {
+        UpdatePosition();
+        UpdateIsMoving();
+    }
+
+    void UpdatePosition()
+    {
+        if (State != CreatureState.Moving) return;
+
+        Vector3 destPos = Managers.Map.CurrentGrid.CellToWorld(CellPos) + new Vector3(0.5f, -0.2f);
+        Vector3 moveDir = destPos - transform.position;
+
+        // 도착 여부 체크
+        float dist = moveDir.magnitude;
+        if (dist < _speed * Time.deltaTime)
+        {
+            transform.position = destPos;
+            // 예외적으로 애니메이션을 직접 컨트롤
+            _state = CreatureState.Idle;
+            if (_dir == MoveDir.None)
+                UpdateAnimation();
+        }
+        else
+        {
+            transform.position += moveDir.normalized * _speed * Time.deltaTime;
+            State = CreatureState.Moving;
+        }
+    }
+
+    void UpdateIsMoving()
+    {
+        if (State == CreatureState.Idle && _dir != MoveDir.None)
+        {
+            Vector3Int destPos = CellPos;
+            switch (_dir)
+            {
+                case MoveDir.Up:
+                    destPos += Vector3Int.up;
+                    break;
+                case MoveDir.Down:
+                    destPos += Vector3Int.down;
+                    break;
+                case MoveDir.Left:
+                    destPos += Vector3Int.left;
+                    break;
+                case MoveDir.Right:
+                    destPos += Vector3Int.right;
+                    break;
+            }
+            State = CreatureState.Moving;
+            if (Managers.Map.CanGo(destPos))
+            {
+                if (Managers.Object.Find(destPos) == null)
+                {
+                    CellPos = destPos;  
+                }
+                
+            }
+        }
+    }
+    
+}
---
+++ Client/Assets/Script/Managers/Contents/ObjectManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManager
+{
+    // 나중에 서버에선 id를 추가해서 딕셔너리로
+    //Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject> ();
+    List<GameObject> _objectes = new List<GameObject>();
+    
+    public void Add(GameObject go)
+    {
+        _objectes.Add(go);
+    }
+
+    public void Remove(GameObject go)
+    {
+        _objectes.Remove(go);
+    }
+
+    // 부하가 엄청나!
+    public GameObject Find(Vector3Int cellPos)
+    {
+        foreach (GameObject obj in _objectes)
+        {
+            CreatureController cc = obj.GetComponent<CreatureController>();
+            if (cc == null) continue;
+
+            if (cc.CellPos == cellPos)
+                return obj;
+        }
+
+        return null;
+    }
+
+    public void Clear()
+    {
+        _objectes.Clear();
+    }
+}
